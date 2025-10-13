2c50ee62dfcf9207cfd4346de1ce0fe69626a728
hamzaslysol-dot <hamza.slysol@gmail.com>
2025-10-13 12:52:28 +0500

    blog page annimated bg

--- src/Components/Blog.tsx
+++ src/Components/Blog.tsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
 import Container from "./container";
 import { BlogView } from "./blogData";
+import Bg from "../assets/bg.mp4";
 
 const Blog = () => {
   return (
-    <div className="bg-black min-h-screen">
-      <Container className="text-white">
-        <h1 className="font-bold text-5xl mb-10">Blogs</h1>
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+      >
+        <source src={Bg} type="video/mp4" />
+      </video>
+      <div className="absolute inset-0 bg-black/50 z-0"></div>
+
+      <Container className="relative z-10 py-10">
+        <h1 className="font-bold text-5xl mb-10 text-center">Blogs</h1>
+
         <div className="grid md:grid-cols-2 gap-5">
           {BlogView.map((blog) => (
             <div
               key={blog.id}
-              className="bg-gray-900 p-5 rounded-3xl shadow-md"
+              className=" backdrop-blur-sm p-5 rounded-3xl shadow-md hover:shadow-lg transition-all duration-300"
             >
               <img
                 src={blog.imgSrc}
@@ -25,7 +38,7 @@ const Blog = () => {
                   : blog.des}
                 <Link
                   to={`/blog/${blog.id}`}
-                  className="text-blue-400 hover:text-blue-300 underline ml-1"
+                  className=" hover:text-blue-300 underline ml-1"
                 >
                   Read more
                 </Link>
